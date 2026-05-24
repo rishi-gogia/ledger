319f25eece38ef85a77258d894d45b5d36421f82
rgogia <rishi.gogia@wisetechglobal.com>
2026-05-24 20:59:00 +0100

    Initial Commit

---
+++ src/test/java/com/teya/ledger/LedgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teya.ledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
