44f723798ed6a88e12b34f49ae490608765b64d1
rgogia <rishi.gogia@wisetechglobal.com>
2026-05-24 22:28:37 +0100

    Added test stubs for TransactionService

---
+++ src/test/java/com/teya/ledger/services/TransactionServiceImplTest.java
@@ -0,0 +1,48 @@
+package com.teya.ledger.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+class TransactionServiceImplTest {
+
+    @Test
+    void testAddTransaction_deposit_updatesBalance() {
+        fail("not implemented");
+    }
+
+    @Test
+    void testAddTransaction_withdrawal_updatesBalance() {
+        fail("not implemented");
+    }
+
+    @Test
+    void testAddTransaction_withdrawal_exactBalance_succeeds() {
+        fail("not implemented");
+    }
+
+    @Test
+    void testAddTransaction_withdrawal_insufficientFunds_throwsException() {
+        fail("not implemented");
+    }
+
+    @Test
+    void testAddTransaction_withdrawal_onEmptyLedger_throwsException() {
+        fail("not implemented");
+    }
+
+    @Test
+    void testGetTransactionList_returnsTransactionList() {
+        fail("not implemented");
+    }
+
+    @Test
+    void testGetTransactionList_returnsEmptyList() {
+        fail("not implemented");
+    }
+
+    @Test
+    void testGetBalance_returnsBalance() {
+        fail("not implemented");
+    }
+}
